24d4c184c3baea5b847d2d18ca06e914a40d3e88
ilahaS <80361058+IlahaS@users.noreply.github.com>
2024-10-18 21:19:30 +0400

    Initial commit

---
+++ BillionerProject/Auth/Controllers/RegisterController.swift
@@ -0,0 +1,81 @@
+//
+//  RegisterController.swift
+//  BillionerProject
+//
+//  Created by Aslanli Faqan on 05.10.24.
+//
+
+import UIKit
+
+protocol RegisterControllerDelegate: AnyObject {
+    func didFinish(user: User)
+}
+final class RegisterController: UIViewController {
+    
+    @IBOutlet private weak var signUpButton: UIButton!
+    @IBOutlet private weak var loginButton: UIButton!
+    @IBOutlet private weak var nameField: UITextField!
+    @IBOutlet private weak var surnameField: UITextField!
+    @IBOutlet private weak var passwordField: UITextField!
+    @IBOutlet private weak var mailField: UITextField!
+    private var user: User?
+    weak var delegate: RegisterControllerDelegate?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    fileprivate func configureUI() {
+        navigationController?.setNavigationBarHidden(true, animated: true)
+        configureButton()
+    }
+    
+    fileprivate func configureButton() {
+        signUpButton.addTarget(self, action: #selector(signUpButtonClicked), for: .touchUpInside)
+        signUpButton.setTitle("Sign Up", for: .normal)
+        loginButton.addTarget(self, action: #selector(loginButtonClicked), for: .touchUpInside)
+        loginButton.setTitle("Login", for: .normal)
+    }
+    
+    @objc
+    fileprivate func signUpButtonClicked() {
+        guard checkValidation() else {
+            print(#function, "fieldler bosh ola bilmez")
+            return
+        }
+        guard let name = nameField.text,
+              let surname = surnameField.text,
+              let password = passwordField.text,
+              let mail = mailField.text else {return}
+        
+        user = User(name: name, surname: surname, email: mail, password: password)
+        
+        saveUserToDefault(user: user)
+        
+        guard let user = user else {return}
+        delegate?.didFinish(user: user)
+        navigationController?.popViewController(animated: true)
+        
+    }
+    
+    fileprivate func saveUserToDefault(user: User?) {
+        UserDefaults.standard.setValue(user?.name, forKey: "name")
+        UserDefaults.standard.setValue(user?.surname, forKey: "surname")
+        UserDefaults.standard.setValue(user?.email, forKey: "email")
+    }
+    
+    fileprivate func checkValidation() -> Bool {
+        guard let name = nameField.text,
+              let surname = surnameField.text,
+              let password = passwordField.text,
+              let mail = mailField.text
+        else {return false}
+        return !(name.isEmpty || surname.isEmpty || password.isEmpty || mail.isEmpty)
+    }
+    
+    
+    @objc
+    fileprivate func loginButtonClicked() {
+        navigationController?.popViewController(animated: true)
+    }
+}
+
